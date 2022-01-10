0100a3f213a7225c49cf0014dac90d00c053c87b
akvt <82463021+akvt@users.noreply.github.com>
2022-01-10 03:04:17 +0100

    Create fibonacci.py
    prints all Fibonacci numbers up to the n-th

---
+++ fibonacci.py
@@ -0,0 +1,15 @@
+def print_fibonacci_numbers(n):
+    old_number = 0
+    number = 1
+    
+    if(n>=0)
+        print(0)
+    
+    for i in range(0,n):
+        print(number)
+        new_number = old_number + number
+        old_number = number
+        number = new_number
+        
+ 
+print(print_fibonacci_numbers(8))
